250e27b3afa91848c2f6c207cb196a0813a745dc
tou-py <sebastiantorresg2003@gmail.com>
2025-10-23 13:46:56 -0300

    chore: crea modelo de Post y establece una relacion con User

---
+++ app/Enums/PostStatus.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Enums;
+
+enum PostStatus: string {
+    case Draft = 'draft';
+    case Published = 'published';
+}
---
+++ database/migrations/2025_10_23_163847_create_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Enums\PostStatus;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+
+            $table->string('title')->unique();
+            $table->text('content');
+            $table->enum('status', array_column(PostStatus::cases(), 'value'))->default(PostStatus::Draft->value);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
